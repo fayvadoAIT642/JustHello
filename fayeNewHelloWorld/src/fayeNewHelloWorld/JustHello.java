961e7aa00b77b102d900f3576ae4054a8eeca49b
fayvado <fayvado@gmail.com>
2014-02-19 13:56:14 -0500

    This is a new class just added

---
+++ fayeNewHelloWorld/src/fayeNewHelloWorld/JustHello.java
@@ -0,0 +1,11 @@
+package fayeNewHelloWorld;
+
+public class JustHello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Just Hello World");
+
+	}
+
+}
