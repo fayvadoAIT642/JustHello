1f57398bd254ad291244217dd6966b4fb1ff6322
fayvado <fayvado@gmail.com>
2014-02-19 13:58:42 -0500

    This is the second commit for this class

--- fayeNewHelloWorld/src/fayeNewHelloWorld/JustHello.java
+++ fayeNewHelloWorld/src/fayeNewHelloWorld/JustHello.java
@@ -5,7 +5,7 @@ public class JustHello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Just Hello World");
-
-	}
+		System.out.println("this is a better message");
+		System.out.println("this is a better message 2");	}
 
 }
