57aa6735e9ef9556bbfabd4c6dc23d7aaacb145f
fayvado <fayvado@gmail.com>
2014-02-19 16:44:28 -0500

    First change to Egithub This just added another print line

--- fayeNewHelloWorld/src/fayeNewHelloWorld/JustHello.java
+++ fayeNewHelloWorld/src/fayeNewHelloWorld/JustHello.java
@@ -2,10 +2,21 @@ package fayeNewHelloWorld;
 
 public class JustHello {
 
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		// This is my second comment 
+		
+		// This is just a test
 		System.out.println("Just Hello World");
 		System.out.println("this is a better message");
-		System.out.println("this is a better message 2");	}
+		System.out.println("this is a better message 2");
+		System.out.println("this is to check a chekin to Egit");
+	}
 
 }
